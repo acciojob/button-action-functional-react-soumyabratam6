10009cc255c88b084bd3fc3e0416d0edb6b2f2a9
soumyabrata <soumyabratam6@gmail.com>
2024-09-02 20:10:07 +0530

    all file added

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[showParagraph,setShowParagraph] = useState(false);
+  const handelSubmit = ()=>{
+    setShowParagraph(true)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={setShowParagraph}>Click Me</button>
+      {
+        showParagraph &&( <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>) 
+      }
     </div>
   );
 }
